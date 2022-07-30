3b58b299fcd17dc907ccc32891bb1c3989abc8cc
Nishanth Samson <80666777+NishanthSamson82@users.noreply.github.com>
2022-07-30 10:46:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-import csv
+
 selected = option_menu("Menu", ["Home",'Tasks','Upload','Settings'],
         icons=['house','list-check','upload', 'gear',], menu_icon="caret-down-fill", default_index=1,orientation="horizontal")
 if(selected == "Tasks"):
